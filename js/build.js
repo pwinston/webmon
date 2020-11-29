fa7cadc53d6fe22a6646ec430c5ceb02e09d429b
Trevor Manz <trevor.j.manz@gmail.com>
2020-11-29 09:55:13 -0500

    Only minify if production build

---
+++ js/build.js
@@ -0,0 +1,14 @@
+require('esbuild').build({
+    entryPoints: [
+        'src/viewer.js',
+        // other files you want to end up in /static
+    ],
+    format: 'esm',
+    outdir: '../static/',
+    bundle: true,
+    splitting: true,
+    minify: process.env.NODE_ENV === 'production', // only minify if production build
+}).catch(err => {
+    console.log(err);
+    process.exit(1);
+})
