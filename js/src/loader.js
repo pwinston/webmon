0973365f4bfe5f0bab73df96f063f01d305ceb6c
Philip Winston <pwinston@gmail.com>
2020-11-30 07:55:43 -0500

    Add connectSocketInput

--- js/src/loader.js
+++ js/src/loader.js
@@ -1,10 +1,38 @@
 //
 // loader.js
 //
+// Graphs about the ChunkLoader.
+//
 import vegaEmbed from 'vega-embed';
 
+export function connectSocketInput() {
+    console.log("connectSocketInput")
+
+    document.addEventListener("DOMContentLoaded", function (event) {
+
+        // Connect invoked when a connection with the server setup.
+        internalParams.socket.on('connect', function () {
+            console.log("connect")
+            internalParams.socket.emit('connection_test', { data: 'loader.js' });
+            internalParams.socket.emit('input_data_request', { data: 'requesting data' });
+        });
+
+        internalParams.socket.on('connection_response', function (msg) {
+            console.log("connection_response:", msg);
+        });
+
+        internalParams.socket.on('input_data_response', function (msg) {
+            console.log("input_data_response", msg);
+        });
+
+        internalParams.socket.on('set_tile_data', function (msg) {
+            setTileData(msg);
+        });
+    });
+}
+
 function showChart() {
-    var yourVlSpec = {
+    var spec = {
         $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
         description: 'A simple bar chart with embedded data.',
         data: {
@@ -26,10 +54,14 @@ function showChart() {
             y: { field: 'b', type: 'quantitative' }
         }
     };
-    vegaEmbed('#vis', yourVlSpec);
+    vegaEmbed('#vis', spec, { defaultStyle: true })
+        .then(function (result) {
+            const view = result.view;
+        }
 }
 
 export function startLoader() {
     console.log("startLoader");
     showChart();
+    connectSocketInput();
 }
