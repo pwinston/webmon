3f7a0b18acfabfb6897017f41badd0ef3fe8aebe
Philip Winston <pwinston@gmail.com>
2020-12-11 22:40:27 -0500

    Add frame_time chart

--- js/src/loader.js
+++ js/src/loader.js
@@ -66,18 +66,28 @@ const load_ms = {
     get_entry: (prev, msg) => ({ x: prev.length, y: msg.load_ms }),
 };
 
+const frame_time = {
+    spec: 'static/specs/frame_time.json',
+    id: "#frame_time",
+    get_entry: (prev, msg) => ({ x: prev.length, y: msg }),
+};
+
 
 export async function startLoader() {
     const bytes_chart = await VegaChart.from_spec(bytes.id, bytes.spec);
     const load_ms_chart = await VegaChart.from_spec(load_ms.id, load_ms.spec);
+    const frame_time_chart = await VegaChart.from_spec(frame_time.id, frame_time.spec);
 
-    params.socket.on('send_load_data', (msg) => {
-        console.log("insert data", msg)
+    params.socket.on('napari_message', (msg) => {
+        if ('load' in msg) {
+            const bytes_entry = bytes.get_entry(bytes_chart.data, msg.load);
+            bytes_chart.push(bytes_entry);
 
-        const bytes_entry = bytes.get_entry(bytes_chart.data, msg);
-        bytes_chart.push(bytes_entry);
-
-        const load_ms_entry = load_ms.get_entry(load_ms_chart.data, msg);
-        load_ms_chart.push(load_ms_entry);
+            const load_ms_entry = load_ms.get_entry(load_ms_chart.data, msg.load);
+            load_ms_chart.push(load_ms_entry);
+        } else if ('frame_time_ms' in msg) {
+            const entry = frame_time.get_entry(frame_time_chart.data, msg.frame_time_ms);
+            frame_time_chart.push(entry);
+        }
     });
 }
